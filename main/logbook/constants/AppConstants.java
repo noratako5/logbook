8c2899a3bf6729899f770a2dff371dc1ca3b5186
biikame <biikame@users.noreply.github.com>
2016-04-22 21:06:16 +0900

    v2.1.8.43

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -24,7 +24,7 @@ public class AppConstants {
     public static final String SUFFIX = "拡張版(赤)";
 
     /** バージョン */
-    public static final String VERSION = "2.1.8.42";
+    public static final String VERSION = "2.1.8.43";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
