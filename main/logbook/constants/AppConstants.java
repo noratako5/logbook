798fdc5914409aa8e7d545d6e2b364143869d814
noratako5 <noratako5@gmail.com>
2016-12-23 10:53:48 +0900

    v2.3.1.44.35

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -24,7 +24,7 @@ public class AppConstants {
     public static final String SUFFIX = "拡張版";
 
     /** バージョン */
-    public static final String VERSION = "2.3.0.44.34";
+    public static final String VERSION = "2.3.1.44.35";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
